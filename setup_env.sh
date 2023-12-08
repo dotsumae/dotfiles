ec353302baede077c25d420f1fee334fdc3543ae
Alex Salazar <33135381+dotsumae@users.noreply.github.com>
2023-12-08 09:45:06 +0100

    Add files via upload

---
+++ setup_env.sh
@@ -0,0 +1,15 @@
+echo "Installing tools..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install trash-cli vim sl zsh tldr autojump wget -y
+echo "Updating user config..."
+cd ~
+wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+echo "# Load autojump" >> .zshrc
+echo ". /usr/share/autojump/autojump.sh" >> .zshrc
+echo "source .alias" >> .zshrc
+echo "alias rm='trash'" >> .alias
+echo 'PATH="/home/alex/Exexutables/bin:$PATH"' >> .zshrc
+echo "Changing shell to zsh..."
+sudo chsh -s /bin/zsh
+echo "zsh" >> .bashrc
+echo "All OK!"
