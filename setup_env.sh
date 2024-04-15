998a962d541408e7be1205f241efa26c4525588b
Alex Salazar <33135381+dotsumae@users.noreply.github.com>
2024-04-15 17:54:36 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -8,10 +8,10 @@ cd ~
 wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 echo "# Load autojump" >> .zshrc
 echo ". /usr/share/autojump/autojump.sh" >> .zshrc
-echo "source .alias" >> .zshrc
+echo "source ~/.alias" >> .zshrc
 echo "alias rm='trash'" >> .alias
 echo "alias update-all='sudo nala upgrade --download-only -y ; sudo apt update && sudo apt upgrade -y'" >> .alias
-mkdir -p Executables/bin/
+mkdir -p ~/Executables/bin/
 echo 'PATH="/home/alex/Executables/bin:$PATH"' >> .zshrc
 echo "Changing shell to zsh..."
 sudo chsh -s /bin/zsh
